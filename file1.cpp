a7867c2c14da47e80026da15d13a64947ddc72dc
Ankit Kumar <ankit.aryan.dude@gmail.com>
2021-10-05 13:26:43 -0500

    Update file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -1,10 +1,7 @@
 #include<bits/stdc++.h>
 #include<math.h>
-int main (hacktoberfest 2020)
+int main (hacktoberfest 2021)
 {
-   cout<<"Let's go #4!"<<"\n";
+   cout<<"Let's go #1!"<<"\n";
 #iostream.h
-
-int main (hacktoberfest 2020){
-cout<<"Let's go #4!"<</n;
 }
